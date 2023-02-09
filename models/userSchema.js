5e6115c179a6eb94d8876d9c6aa1623cd1d12bb2
Abhay0122 <parmarabhay2201@gmail.com>
2023-02-10 01:30:05 +0530

    third commit

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,18 +2,19 @@ const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: String,
-    password: String,
-    dob: String,
-    gender:{
-      type:String, 
-    }, 
-    avatar: {
-        type: String,
-        default: "dummy.png",
-    },
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String,
+  dob: String,
+  gender: String,
+  phone: String,
+  city: String,
+  avatar: {
+    type: String,
+    default: "dummy.png",
+  },
+  post: []
 });
 
 userSchema.plugin(plm, { usernameField: "email" });
